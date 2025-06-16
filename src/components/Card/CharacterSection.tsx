5e64f5361084ca2e98b1678b12c82a635a58173c
Dominic A <dominic.aks01@gmail.com>
2025-06-16 16:33:35 -0700

    add cdn back

--- src/components/Card/CharacterSection.tsx
+++ src/components/Card/CharacterSection.tsx
@@ -201,7 +201,7 @@ export const CharacterSection: React.FC<CharacterSectionProps> = ({
     if (customImage) {
       return imageUrl ?? undefined;
     }
-    return getAssetPath('icons', character, useAltSkin).local;
+    return getAssetPath('icons', character, useAltSkin).cdn;
   }, [character, customImage, imageUrl, useAltSkin]);
 
   const imageStyle = useMemo(() => ({
