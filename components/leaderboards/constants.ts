5314c81206a308a2787fa0cca32a223b6ae1637e
Dominic A <dominic.aks01@gmail.com>
2026-06-12 00:28:06 -0700

    add slightly more spacing to sets to let 1p (3 total sets) have more breathing room

--- components/leaderboards/constants.ts
+++ components/leaderboards/constants.ts
@@ -150,7 +150,7 @@ export const SEQUENCE_BADGE_STYLES = [
 ] as const;
 
 // # | Owner | Character | Sets | [CV+Stats+Damage]
-export const LB_TABLE_GRID = 'grid-cols-[48px_178px_178px_88px_minmax(0,1fr)]';
+export const LB_TABLE_GRID = 'grid-cols-[48px_178px_172px_94px_minmax(0,1fr)]';
 export const LB_SORTABLE_GROUP_GRID = 'grid-cols-[172px_repeat(4,121px)_minmax(140px,1fr)]';
 export const DEFAULT_LB_SORT = 'damage';
 export const DEFAULT_LB_TRACK = 's0';
