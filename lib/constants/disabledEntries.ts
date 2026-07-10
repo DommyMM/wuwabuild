eb1e330ba9c4692a78da4c610cf4a0882746bfd8
Dominic A <dominic.aks01@gmail.com>
2026-07-10 12:43:26 -0700

    enable suisui

--- lib/constants/disabledEntries.ts
+++ lib/constants/disabledEntries.ts
@@ -1,7 +1,7 @@
 export const DISABLED_CHARACTER_IDS: ReadonlySet<string> = new Set<string>([
-    '1110'
+    
 ]);
 
 export const DISABLED_WEAPON_IDS: ReadonlySet<string> = new Set<string>([
-    '21050096'
+
 ]);
